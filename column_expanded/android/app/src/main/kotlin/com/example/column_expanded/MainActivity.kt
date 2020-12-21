5451fe945fa5ade383e9f952cb311a60a625f6e9
seyha30 <57175099+seyha30@users.noreply.github.com>
2020-12-21 20:17:22 +0700

    comit

---
+++ column_expanded/android/app/src/main/kotlin/com/example/column_expanded/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.column_expanded
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ column_expanded/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    RawMaterialButton redButton = RawMaterialButton(
+      onPressed: () => debugPrint('Red Button Pressed'),
+      constraints: BoxConstraints(maxHeight: 136, minWidth: 188.0),
+      shape: CircleBorder(),
+      fillColor: Colors.red,
+      elevation: 2.0,
+      padding: EdgeInsets.all(15.0),
+    );
+    RawMaterialButton blueButton = RawMaterialButton(
+      onPressed: () => debugPrint('Red Button Pressed'),
+      constraints: BoxConstraints(maxHeight: 136.0, minWidth: 188),
+      shape: CircleBorder(),
+      fillColor: Colors.blue,
+      elevation: 2.0,
+      padding: EdgeInsets.all(15.0),
+    );
+    RawMaterialButton greenButton = RawMaterialButton(
+      onPressed: () => debugPrint('Red Button Pressed'),
+      constraints: BoxConstraints(maxHeight: 136.0, minWidth: 188),
+      shape: CircleBorder(),
+      fillColor: Colors.green,
+      elevation: 2.0,
+      padding: EdgeInsets.all(15.0),
+    );
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(child: redButton),
+          Expanded(child: greenButton),
+          Expanded(child: blueButton),
+        ],
+      ),
+    ));
+  }
+}
