06601a818d120e2306c1c59fee0be2ffe5dfe217
seyha30 <57175099+seyha30@users.noreply.github.com>
2020-12-21 20:46:04 +0700

    RowMainAxisAlignment

---
+++ row_main_axis_alignment/android/app/src/main/kotlin/com/example/row_main_axis_alignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.row_main_axis_alignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ row_main_axis_alignment/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(RowMainAxisAlignment());
+
+class RowMainAxisAlignment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: MyHome(),
+      ),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center, children: [
+                const Text("MainAxisAlignment"),
+                const Text('is'),
+                const Text('center'),
+              ]),
+              Row(
+                mainAxisAlignment:MainAxisAlignment.end,
+                children: [
+                  const Text("MainAxisAlignment"),
+                  const Text('is'),
+                  const Text('end'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment:MainAxisAlignment.spaceAround,
+                children: [
+                  const Text("MainAxisAlignment"),
+                  const Text('is'),
+                  const Text('space around'),
+                ],
+              ) ,
+              Row(
+                mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text("MainAxisAlignment"),
+                  const Text('is'),
+                  const Text('spaceEvenly'),
+                ],
+              ),
+               Row(
+                mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("MainAxisAlignment"),
+                  const Text('is'),
+                  const Text('spaceBetween'),
+                ],
+              ),
+                             Row(
+                mainAxisAlignment:MainAxisAlignment.start,
+                children: [
+                  const Text("MainAxisAlignment"),
+                  const Text('is'),
+                  const Text('spaceBetween'),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
